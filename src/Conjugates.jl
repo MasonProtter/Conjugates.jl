c5ae5cc77937168498f5393a8267db93e8d5ba41
MasonProtter <mason.protter@icloud.com>
2020-04-03 13:40:56 -0600

    add tp, update readme

--- src/Conjugates.jl
+++ src/Conjugates.jl
@@ -1,15 +1,39 @@
 module Conjugates
 
-using LinearAlgebra
+using LinearAlgebra: LinearAlgebra, Symmetric, Hermitian
 
 struct HC end
-const hc = HC() #hermitian conjugate
+const hc = HC()
+
+struct TP end
+const tp = TP()
 
 Base.:(+)(x, ::HC) = x + x'
-Base.:(+)(x::Number, ::HC) = 2real(x)
+Base.:(+)(x::Union{Real, Complex}, ::HC) = 2real(x)
 Base.:(+)(x::Hermitian, ::HC) = 2x
 Base.:(+)(x::AbstractMatrix, ::HC) = Hermitian(x + x')
 
-export hc
+Base.:(-)(x, ::HC) = x - x'
+Base.:(-)(x::Number, ::HC) = 2imag(x)
+
+Base.:(*)(x, ::HC) = x * x'
+Base.:(*)(x::Complex, ::HC) = real(x)^2 + imag(x)^2
+Base.:(*)(x::Hermitian, ::HC) = x^2
+Base.:(*)(x::AbstractMatrix, ::HC) = Hermitian(x * x')
+
+
+Base.:(+)(x, ::TP) = x + transpose(x)
+Base.:(+)(x::Number, ::TP) = 2x
+Base.:(+)(x::Symmetric, ::TP) = 2x
+Base.:(+)(x::AbstractMatrix, ::TP) = Symmetric(x + x')
+
+Base.:(-)(x, ::TP) = x - transpose(x)
+Base.:(-)(x::Number, ::TP) = zero(x)
+
+Base.:(*)(x, ::TP) = x * transpose(x)
+Base.:(*)(x::Symmetric, ::TP) = x^2
+Base.:(*)(x::AbstractMatrix, ::TP) = Symmetric(x * x')
+
+export hc, tp
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 
-using PlusHC, Test, LinearAlgebra
+using Conjugates, Test, LinearAlgebra
 
 x = 1 + im
 A = randn(ComplexF64, 100, 100)
@@ -8,3 +8,10 @@ A = randn(ComplexF64, 100, 100)
 @test A + hc ≈ A + A'
 @test ishermitian(A + hc)
 @test (A + hc) + hc ≈ 2(A + hc)
+@test A*hc ≈ A*A'
+
+@test x + tp == 2x
+@test A + tp ≈ A + transpose(A)
+@test isymmetric(A + tp)
+@test (real(A) + tp) + tp ≈ 2(real(A) + tp)
+@test A*tp ≈ A*transpose(A)
